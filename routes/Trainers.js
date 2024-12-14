085ba5118b0984dcf55c9c202c4f67b46dde6daa
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-12-14 18:13:14 +0600

    Trainers Query Created

--- routes/Trainers.js
+++ routes/Trainers.js
@@ -1,14 +1,71 @@
+const { client } = require("../config/db");
 const express = require("express");
 const router = express.Router();
-const { client } = require("../config/db");
 
 // Collection for Trainers
 const TrainersCollection = client.db("Seven-Gym").collection("Trainers");
 
 // Get Trainers
+// Assuming you're using MongoDB (or a similar DB that supports this syntax)
 router.get("/", async (req, res) => {
   try {
-    const result = await TrainersCollection.find().toArray();
+    const {
+      name,
+      specialization,
+      tier,
+      gender,
+      experienceMin,
+      experienceMax,
+      feeMin,
+      feeMax,
+      languages,
+      classType,
+      focusArea,
+    } = req.query;
+
+    const query = {};
+
+    // Add filters to the query only if they are defined
+    if (name) {
+      query.name = { $regex: new RegExp(name, "i") }; // Partial match for name
+    }
+    if (specialization) {
+      query.specialization = specialization;
+    }
+    if (tier) {
+      query.tier = tier;
+    }
+    if (gender) {
+      query.gender = gender;
+    }
+    if (experienceMin || experienceMax) {
+      query.experience = {
+        ...(experienceMin && { $gte: parseInt(experienceMin) }),
+        ...(experienceMax && { $lte: parseInt(experienceMax) }),
+      };
+    }
+    if (feeMin || feeMax) {
+      query.fee = {
+        ...(feeMin && { $gte: parseFloat(feeMin) }),
+        ...(feeMax && { $lte: parseFloat(feeMax) }),
+      };
+    }
+    if (languages) {
+      query.languages = { $in: languages.split(",") };
+    }
+    if (classType) {
+      query["preferences.classTypes"] = { $in: classType.split(",") };
+    }
+    if (focusArea) {
+      query["preferences.focusAreas"] = { $in: focusArea.split(",") };
+    }
+
+    // If no filters are applied, return all trainers
+    if (Object.keys(query).length === 0) {
+      return res.send(await TrainersCollection.find().toArray());
+    }
+
+    const result = await TrainersCollection.find(query).toArray();
     res.send(result);
   } catch (error) {
     console.error("Error fetching Trainers:", error);
