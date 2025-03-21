9e4d46b1cd0c987c8aa3a1be56334b39ff951a56
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2025-03-21 12:29:36 +0600

    Add search functionality for Tier_Upgrade_Refund by RefundID, _id, or email

--- routes/Payment/TierUpgradeRefund.js
+++ routes/Payment/TierUpgradeRefund.js
@@ -18,6 +18,31 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Search Tier_Upgrade_Refund by RefundID, _id, or email
+router.get("/search", async (req, res) => {
+  try {
+    const { refundID, _id, email } = req.query;
+    let query = {};
+
+    if (refundID) {
+      query.RefundID = refundID;
+    }
+    if (_id) {
+      // Convert _id string to ObjectId
+      query._id = ObjectId(_id);
+    }
+    if (email) {
+      query.email = email;
+    }
+
+    const result = await Tier_Upgrade_RefundCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error searching Tier_Upgrade_Refund:", error);
+    res.status(500).send("Something went wrong.");
+  }
+});
+
 // POST Tier_Upgrade_Refund
 router.post("/", async (req, res) => {
   try {
